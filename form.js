10765ae40b0039f5afb5c9a20310b7769d307615
yuki1601030 <yuki_hirata_ab@mail.toyota.co.jp>
2025-07-31 08:33:07 +0900

    Create form.js

---
+++ form.js
@@ -0,0 +1,21 @@
+document.getElementById("information").addEventListener("submit" , 
+            function(event) {
+              event.preventDefault();//フォームの送信を止める(jsで処理したいから)
+
+
+const lastName = document.getElementById("lastName").value;
+const firstName = document.getElementById("firstName").value;
+const age = document.getElementById("age").value;
+
+if (lastName && firstName && age) {
+    //ユーザー情報をローカルストレージに保存
+    localStorage["username"] = localStorage[{"lastName" : lastName , "firstName" : firstName , "age" : age}]
+    // localStorage.setItem("lastName" , lastName);
+    // localStorage.setItem("firstName" , firstName);
+    // localStorage.setItem("age" , age);
+    alert("登録が完了しました！ログイン画面に戻ります！")
+    window.location.href = "index.html"
+} else {
+    alert("未入力です")
+}
+});
